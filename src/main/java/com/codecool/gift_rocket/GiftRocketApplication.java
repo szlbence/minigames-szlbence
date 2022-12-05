71d1441a3a966b75d3165e93547f70386c4c0113
Andras Csovari <csovaria@gmail.com>
2022-12-05 14:31:49 +0100

    setup project

---
+++ src/main/java/com/codecool/gift_rocket/GiftRocketApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.gift_rocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiftRocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiftRocketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codecool/gift_rocket/GiftRocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.gift_rocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiftRocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
