d4e0183d2adc4849eefd6e9b93d8377863287e4b
NagyGabor87 <gabor.nagy.aiesec@gmail.com>
2023-01-10 09:19:50 +0100

    create react frontend for project

--- src/main/java/com/codecool/gift_rocket/controller/CartController.java
+++ src/main/java/com/codecool/gift_rocket/controller/CartController.java
@@ -57,4 +57,9 @@ public class CartController {
     public void removeProductBoxFromCart(@PathVariable UUID id, @PathVariable UUID productBoxId) {
         shopService.removeProductBoxFromCart(productBoxId,id);
     }
+
+    @GetMapping("/api/dadjokes")
+    public String dadJokes() {
+        return "Justice is a dish best served cold, if it were served warm it would be just water.";
+    }
 }
