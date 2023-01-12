b147c532421db07f12f9f509e9f1cba033396a92
NagyGabor87 <gabor.nagy.aiesec@gmail.com>
2023-01-12 09:09:02 +0100

    add react-bootstrap navbar and footer

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,33 +1,43 @@
 import React from 'react';
 import "./HomePage.css";
 import image from "../pexels-andrea-piacquadio-1050256.jpg"
+import Card from 'react-bootstrap/Card';
 
 //logo, login, image, description
 const Home = () => {
     return (
-        <div className="page">
-            <img className="homeImg" src={image}/>
-            <h1>Welcome to GiftRocket</h1>
-            <h2>Our company is established to make sure that every person can
-                choose their desired gifts which can be given to their person of choice. </h2>
-            <div className="grid">
-                <div className="card">
-                    <img/>
-                    <div className="container">
-                        <h4>Pre-made Boxes</h4>
-                        <p>If you don't have a specific idea about your gift</p>
-                    </div>
-                </div>
-                <div className="card">
-                    <img/>
-                    <div className="container">
-                        <h4>Custom Boxes</h4>
-                        <p>You can create your own gift box here</p>
-                    </div>
+        <div className="main">
+            <div className="image">
+                <img className="homeImg" src={image} style={{objectFit: "cover", width: 2000}}/>
+            </div>
+            <div className="page">
+                <h1 align="center" style={{fontSize: 50}}>Welcome to GiftRocket</h1>
+            <br/>
+                <p style={{fontSize: 35}}>Our company is established to make sure that every person can
+                choose their desired gifts which can be given to their person of choice</p>
+                <div className="grid">
+                <Card style={{ width: '36rem' }}>
+                   <Card.Header><Card.Img variant="top" src="holder.js/100px180" /></Card.Header>
+                    <Card.Body>
+                        <Card.Title>Pre-made Boxes</Card.Title>
+                        <Card.Text>
+                            If you don't have a specific idea about your gift
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+                <Card style={{ width: '36rem' }}>
+                    <Card.Header><Card.Img variant="top" src="holder.js/100px180" /></Card.Header>
+                    <Card.Body>
+                        <Card.Title>Custom Boxes</Card.Title>
+                        <Card.Text>
+                            You can create your own gift box
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
                 </div>
             </div>
         </div>
-            );
-            };
+    );
+};
 
-            export default Home;
+export default Home;
