acfd36ae935907ae61cb4a38a0484c24087393ee
NagyGabor87 <gabor.nagy.aiesec@gmail.com>
2023-01-25 11:01:00 +0100

    make changes to ProductController and ProductService

--- src/main/java/com/codecool/gift_rocket/controller/ProductController.java
+++ src/main/java/com/codecool/gift_rocket/controller/ProductController.java
@@ -1,59 +1,46 @@
-//package com.codecool.gift_rocket.controller;
-//
-//import com.codecool.gift_rocket.model.Product;
-//import com.codecool.gift_rocket.model.ProductBox;
-//import com.codecool.gift_rocket.service.ProductBoxService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.*;
-//
-//import java.math.BigDecimal;
-//import java.util.List;
-//
-//
-//@RestController
-//@RequestMapping("/productbox")
-//public class NewProductBoxController {
-//    @Autowired
-//    public ProductBoxService productBoxService;
-//
-//    @GetMapping
-//    public List<ProductBox> getAllProductBoxes() {
-//        return  productBoxService.getAllProductBoxes();
-//    }
-//
-//
-//    @PostMapping
-//    public void addProductBox(@RequestBody ProductBox productBox) {
-//        productBoxService.addNewProductBox(productBox);
-//    }
-//
-//    @GetMapping("/{productBoxId}")
-//    public ProductBox findProductBoxById(@PathVariable Long productBoxId) {
-//        return productBoxService.findProductBoxById(productBoxId);
-//    }
-//
-//    @DeleteMapping("/{productBoxId}")
-//    public void removeProductBoxById(@PathVariable Long productBoxId) {
-//        productBoxService.removeProductBoxById(productBoxId);
-//    }
-//
-//    @GetMapping("/{productBoxId}/value")
-//    public BigDecimal getProductBoxValue(@PathVariable Long productBoxId) {
-//        return productBoxService.getProductBoxValue(productBoxId);
-//    }
-//
-//    @GetMapping("/{productBoxId}/products")
-//    public List<Product> getAllProductsInBox(@PathVariable Long productBoxId) {
-//        return productBoxService.getAllProductsInBox(productBoxId);
-//    }
-//
-//    @PostMapping("/{productBoxId}/add/{productId}")
-//    public void addProductToProductBox(@PathVariable Long productBoxId, @PathVariable Long productId) {
-//        productBoxService.addProductToProductBox(productId, productBoxId);
-//    }
-//
-//    @DeleteMapping("/{productBoxId}/remove/{productId}")
-//    public void removeProductFromProductBox(@PathVariable Long productBoxId, @PathVariable Long productId) {
-//        productBoxService.removeProductFromProductBox(productId, productBoxId);
-//    }
-//}
+package com.codecool.gift_rocket.controller;
+
+import com.codecool.gift_rocket.model.Category;
+import com.codecool.gift_rocket.model.Product;
+import com.codecool.gift_rocket.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+    @Autowired
+    public ProductService productService;
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return  productService.getAllProducts();
+    }
+    @PostMapping
+    public void addProduct(@RequestBody Product product) {
+        productService.addNewProduct(product);
+    }
+
+    @GetMapping("/{productId}")
+    public Product findProductById(@PathVariable Long productId) {
+        return productService.findProductById(productId);
+    }
+
+    @DeleteMapping("/{productId}")
+    public void removeProductById(@PathVariable Long productId) {
+        productService.removeProductById(productId);
+    }
+
+    @GetMapping("/{productId}/value")
+    public BigDecimal getProductValue(@PathVariable Long productId) {
+        return productService.getProductValue(productId);
+    }
+    @GetMapping("/category/{category}")
+    public List<Product> getAllProductsInBox(@PathVariable Category category) {
+        return productService.getProductsByCategory(category);
+    }
+}
--- src/main/java/com/codecool/gift_rocket/service/ProductService.java
+++ src/main/java/com/codecool/gift_rocket/service/ProductService.java
@@ -1,159 +1,60 @@
-//package com.codecool.gift_rocket.service;
-//
-//import com.codecool.gift_rocket.model.Cart;
-//import com.codecool.gift_rocket.model.Product;
-//import com.codecool.gift_rocket.model.ProductBox;
-//import com.codecool.gift_rocket.repository.JPA.ProductBoxRepository;
-//import com.codecool.gift_rocket.repository.JPA.ProductRepository;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-//
-//import java.math.BigDecimal;
-//import java.util.List;
-//import java.util.NoSuchElementException;
-//import java.util.Optional;
-//
-//@Service
-//public class ProductBoxService {
-//    @Autowired
-//    ProductBoxRepository productBoxRepository;
-//    @Autowired
-//    ProductRepository productRepository;
-//    public List<ProductBox> getAllProductBoxes() {
-//        return (List<ProductBox>) productBoxRepository.findAll();
-//    }
-//
-//    public void addNewProductBox(ProductBox productBox) {
-//        productBox.setTotalPrice(productBox.getPackagingPrice());
-//        productBoxRepository.save(productBox);
-//    }
-//
-//    public ProductBox findProductBoxById(Long productBoxId) {
-//        Optional<ProductBox> foundProductBox = productBoxRepository.findById(productBoxId);
-//        if(foundProductBox.isPresent()){
-//            return foundProductBox.get();
-//        }
-//        else {
-//            throw new NoSuchElementException("No product box found by given id");
-//        }
-//    }
-//
-//    public void removeProductBoxById(Long productBoxId) {
-//        Optional<ProductBox> foundProductBox = productBoxRepository.findById(productBoxId);
-//        if(foundProductBox.isPresent()){
-//            productBoxRepository.delete(foundProductBox.get());
-//        }
-//        else {
-//            throw new NoSuchElementException("No product box found by given id");
-//        }
-//    }
-//
-//    public BigDecimal getProductBoxValue(Long productBoxId) {
-//        Optional<ProductBox> foundProductBox = productBoxRepository.findById(productBoxId);
-//        if(foundProductBox.isPresent()){
-//            return foundProductBox.get().getTotalPrice();
-//        }
-//        else {
-//            throw new NoSuchElementException("No product box found by given id");
-//        }
-//    }
-//
-//    public List<Product> getAllProductsInBox(Long productBoxId) {
-//        Optional<ProductBox> foundProductBox = productBoxRepository.findById(productBoxId);
-//        if(foundProductBox.isPresent()){
-//            return foundProductBox.get().getProducts();
-//        }
-//        else {
-//            throw new NoSuchElementException("No product box found by given id");
-//        }
-//    }
-//
-//    public void addProductToProductBox(Long productId, Long productBoxId) {
-//        Optional<ProductBox> foundProductBox = productBoxRepository.findById(productBoxId);
-//        Optional<Product> foundProduct = productRepository.findById(productId);
-//        if(foundProductBox.isPresent()){
-//            if(foundProduct.isPresent()){
-//                foundProductBox.get().addProduct(foundProduct.get());
-//                productBoxRepository.save(foundProductBox.get());
-//            }
-//            else {
-//                throw new NoSuchElementException("No product box found by given id");
-//            }
-//        }
-//        else {
-//            throw new NoSuchElementException("No cart found by given id");
-//        }
-//    }
-//
-//    public void removeProductFromProductBox(Long productId, Long productBoxId) {
-//        Optional<ProductBox> foundProductBox = productBoxRepository.findById(productBoxId);
-//        Optional<Product> foundProduct = productRepository.findById(productId);
-//        if(foundProductBox.isPresent()){
-//            if(foundProduct.isPresent()){
-//                foundProductBox.get().removeProduct(foundProduct.get());
-//                productBoxRepository.save(foundProductBox.get());
-//            }
-//            else {
-//                throw new NoSuchElementException("No product box found by given id");
-//            }
-//        }
-//        else {
-//            throw new NoSuchElementException("No cart found by given id");
-//        }
-//    }
-//}
+package com.codecool.gift_rocket.service;
 
+import com.codecool.gift_rocket.model.Category;
+import com.codecool.gift_rocket.model.Product;
+import com.codecool.gift_rocket.repository.JPA.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
+@Service
+public class ProductService {
+    @Autowired
+    ProductRepository productRepository;
+    public List<Product> getAllProducts() {
+        return (List<Product>) productRepository.findAll();
+    }
 
-// These were in the old ProductService class
+    public void addNewProduct(Product product) {
+        productRepository.save(product);
+    }
 
-//package com.codecool.gift_rocket.service;
-//
-//import com.codecool.gift_rocket.model.Category;
-//import com.codecool.gift_rocket.model.Product;
-//import com.codecool.gift_rocket.model.ProductBox;
-//import com.codecool.gift_rocket.repository.JPA.ProductRepository;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-//
-//import java.util.List;
-//import java.util.NoSuchElementException;
-//import java.util.Optional;
-//
-//@Service
-//public class ProductService {
-//    @Autowired
-//    ProductRepository productRepository;
-//    public List<Product> getAllProducts() {
-//        return (List<Product>) productRepository.findAll();
-//    }
-//
-//    public void addNewProduct(Product newProduct) {
-//        productRepository.save(newProduct);
-//    }
-//
-//    public Product findProductById(Long productId) {
-//        Optional<Product> foundProduct = productRepository.findById(productId);
-//        if(foundProduct.isPresent()){
-//            return foundProduct.get();
-//        }
-//        else {
-//            throw new NoSuchElementException("No product found by given id");
-//        }
-//    }
-//
-//    public void removeProductById(Long productId) {
-//        Optional<Product> foundProduct = productRepository.findById(productId);
-//        if(foundProduct.isPresent()){
-//            productRepository.deleteById(productId);
-//        }
-//        else {
-//            throw new NoSuchElementException("No product found by given id");
-//        }
-//    }
-//
-//    public List<Product> getProductsByCategory(Category category) {
-//        return productRepository.findAllByCategory(category);
-//    }
-//}
+    public Product findProductById(Long productId) {
+        Optional<Product> foundProduct = productRepository.findById(productId);
+        if(foundProduct.isPresent()){
+            return foundProduct.get();
+        }
+        else {
+            throw new NoSuchElementException("No product found by given id");
+        }
+    }
+
+    public void removeProductById(Long productId) {
+        Optional<Product> foundProduct = productRepository.findById(productId);
+        if(foundProduct.isPresent()){
+            productRepository.delete(foundProduct.get());
+        }
+        else {
+            throw new NoSuchElementException("No product found by given id");
+        }
+    }
+
+    public BigDecimal getProductValue(Long productId) {
+        Optional<Product> foundProduct = productRepository.findById(productId);
+        if(foundProduct.isPresent()){
+            return foundProduct.get().getPrice();
+        }
+        else {
+            throw new NoSuchElementException("No product found by given id");
+        }
+    }
+
+    public List<Product> getProductsByCategory(Category category) {
+        return productRepository.findAllByCategory(category);
+    }
+}
+
