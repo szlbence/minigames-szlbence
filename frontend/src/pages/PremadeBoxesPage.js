d84b5b47cc7d3a5a2cccefe393b26f4d4b05f924
NagyGabor87 <gabor.nagy.aiesec@gmail.com>
2023-01-26 09:29:28 +0100

    start to fix logic between frontend and backend

--- frontend/src/pages/PremadeBoxesPage.js
+++ frontend/src/pages/PremadeBoxesPage.js
@@ -11,29 +11,29 @@ const PremadeBoxes = () => {
     const [isLoaded, setIsLoaded] = useState(false);
     const [items, setItems] = useState([]);
     const [carts, setCarts] = useState([])
-    const PRODUCTBOX_URL = "/productbox";
+    const PRODUCT_URL = "/product";
     // const PRODUCTBOX_NAME_URL = "/productbox/name";
     const CART_URL = "/cart";
 
-    async function AddToCart(cartId, productBoxId) {
+    async function AddToCart(cartId, productId) {
         //No need for id fetching anymore!
         // const productBoxId = await DataService.getProductBoxId(PRODUCTBOX_NAME_URL, name);
-        await DataService.postData(`${CART_URL}/${cartId}/add/${productBoxId}`)
+        await DataService.postData(`${CART_URL}/${cartId}/add/${productId}`)
     }
     async function getCarts() {
         const carts = await DataService.getData(CART_URL);
         setCarts(carts.data);
     }
 
-    async function getProductBoxes() {
-        const productBoxes = await DataService.getData(PRODUCTBOX_URL);
+    async function getProducts() {
+        const products = await DataService.getData(PRODUCT_URL);
         setIsLoaded(true);
-        setItems(productBoxes.data);
+        setItems(products.data);
     }
 
     useEffect(() => {
         getCarts();
-        getProductBoxes();
+        getProducts();
     }, [])
 
     if (!isLoaded) {
@@ -47,9 +47,8 @@ const PremadeBoxes = () => {
                             <Card.Header></Card.Header>
                             <Card.Body>
                                 <Card.Title>{item.name}</Card.Title>
-                                <p className="price">Total price: {item.totalPrice}</p>
+                                <p className="price">Total price: {item.price}</p>
                                 <p className="description">Description: {item.description}</p>
-                                <p className="products">Products: {item.products.map(product => product.name).join(", ")}</p>
                                 <button type="submit" onClick={() => {AddToCart(carts[0].id, item.id)}}>Add To Cart</button>
                             </Card.Body>
                         </Card>
