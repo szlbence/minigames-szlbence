163cc736bbc3b7890af8ab03a27657cbf7ca61a4
danieldudas91 <duddaniel0919@gmail.com>
2023-02-10 15:27:55 +0100

    implement corsConfigurer bean

--- src/main/java/com/codecool/gift_rocket/GiftRocketApplication.java
+++ src/main/java/com/codecool/gift_rocket/GiftRocketApplication.java
@@ -3,7 +3,10 @@ package com.codecool.gift_rocket;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class GiftRocketApplication {
@@ -15,6 +18,16 @@ public class GiftRocketApplication {
 	@Bean
 	public BCryptPasswordEncoder bCryptPasswordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:3000").allowCredentials(true);
+			}
+		};
 	}
 
 }
