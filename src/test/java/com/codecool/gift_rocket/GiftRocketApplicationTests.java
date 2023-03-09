9de808ffc08a0d2a7b100e61b692ec890a4011de
Bence Szlavik <szlbence@gmail.com>
2023-03-09 11:49:17 +0100

    Check if github actions runs unit tests1 [test]

--- src/test/java/com/codecool/gift_rocket/GiftRocketApplicationTests.java
+++ src/test/java/com/codecool/gift_rocket/GiftRocketApplicationTests.java
@@ -3,11 +3,21 @@ package com.codecool.gift_rocket;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class GiftRocketApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void githubActionsResponseOK(){
+		String text = "OK";
+		assertEquals("OK", text);
 	}
 
 }
